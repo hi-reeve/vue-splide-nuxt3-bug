a938317e11955a82ce53fc6547bb595d5a00640f
Christopher Reeve <reeveworkmail@gmail.com>
2022-03-01 15:00:04 +0700

    initial commit

---
+++ app.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+import { Splide, SplideSlide } from "@splidejs/vue-splide";
+import "@splidejs/splide/dist/css/themes/splide-default.min.css";
+</script>
+<template>
+    <div>
+        <Splide :options="{ rewind: true }">
+            <SplideSlide>
+                <img src="https://source.unsplash.com/random" alt="Sample 1" />
+            </SplideSlide>
+            <SplideSlide>
+                <img src="https://source.unsplash.com/random" alt="Sample 2" />
+            </SplideSlide>
+        </Splide>
+    </div>
+</template>
